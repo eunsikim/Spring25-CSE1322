f186adb292bfbc0bfa3b94cb85a625402d04ceea
eunsikim <kimeunsik229@gmail.com>
2025-01-14 15:13:47 -0500

    Added class excercise solution

---
+++ Week-2/classExcercise1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class classExcercise1 {
+    public static void main(String[] args) {
+        double amount;
+        double rate;
+        int years;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the investment amount: ");
+        amount = sc.nextDouble();
+
+        System.out.print("Enter the rate (enter as decimal): ");
+        rate = sc.nextDouble();
+
+        System.out.print("Enter year: ");
+        years = sc.nextInt();
+
+        for(int i = 0; i < years; i++){
+            System.out.println(amount + " += " + (amount * rate));
+            amount += amount * rate;
+        }
+
+        System.out.println(years + " have passed, you now have $" + amount);
+    }
+}
