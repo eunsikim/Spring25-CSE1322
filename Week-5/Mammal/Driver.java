3edb93880f271b986d158d32a87d75ddd73b9337
eunsikim <kimeunsik229@gmail.com>
2025-02-04 15:00:47 -0500

    Week 5 - Inheritance Mammal Example

--- Week-5/Mammal/Driver.java
+++ Week-5/Mammal/Driver.java
@@ -49,11 +49,23 @@ class Dog extends Mammal{
     }
 }
 
+class Labrador extends Dog{
+    public Labrador (float temp, float weight, int IQ, String furColor, int BarkVolume){
+        super(temp, weight, IQ, furColor, BarkVolume);
+    }
+}
+
 public class Driver {
     public static void main(String[] args) {
         // 38 celsius, 20 kg, 100 IQ, Brown Fur, 100dB bark
         Dog d1 = new Dog(38f, 20, 100, "Brown", 100);
 
+        Mammal m1 = new Mammal(38f, 20, 100, "Brown");
+
+        Labrador l1 = new Labrador(38f, 20, 100, "Brown", 100);
+
+        m1.Eat();
         d1.Eat();
+        l1.Eat();
     }
 }
