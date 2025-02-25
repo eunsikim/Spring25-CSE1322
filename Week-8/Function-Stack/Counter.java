7354d492da7a58e6770530e5cf96416aabeb8c69
eunsikim <kimeunsik229@gmail.com>
2025-02-25 14:38:05 -0500

    Week 8

--- Week-8/Function-Stack/Counter.java
+++ Week-8/Function-Stack/Counter.java
@@ -1,20 +1,19 @@
 public class Counter {
     public static void count(int n){
-        // Base case
+        // Base
         if(n == -1){
             return;
         }
         
-        // Recursive call
-        count(n-1);
-        
         System.out.println("Count: " + n);
-    } 
+
+        // Recursive Call
+        count(n - 1);
+    }
     public static void main(String[] args) {
         int number = 5;
-
         // for(int i = 0; i <= number; i++){
-        //     System.out.println("Count: " + i);
+        //     System.out.println("Count: " + (number - i));
         // }
 
         count(number);
